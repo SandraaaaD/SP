20e61ea1dad8bd508c487b42e7003f9ac4a7a86a
SandraaaaD <sandradamceva@gmail.com>
2025-10-20 17:30:06 +0200

    Create 5.cpp

---
+++ Lab1/5.cpp
@@ -0,0 +1,13 @@
+//vnesi dva broevi; napraj zamena; da se ispecati razlika pred i posle zamena;
+#include <iostream>
+using namespace std;
+int main()
+{
+    float a,b;
+    cout<<"Vnesi dva broja"<<endl;
+    cin>>a>>b;
+    cout<<"Nivnata razlika e: "<<a-b<<endl;
+    cout<<"Nivnata razlika posle zamenata e: "<<b-a<<endl;
+    return 0;
+}
+
