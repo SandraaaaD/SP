9bab21c8ca2f3ce9cf5deabe46c1effb048fbabd
SandraaaaD <sandradamceva@gmail.com>
2025-10-20 17:47:14 +0200

    Create 3.cpp

---
+++ Lab3/3.cpp
@@ -0,0 +1,21 @@
+//telefonski povik
+#include <iostream>
+using namespace std;
+int main()
+{
+    int br1,br2,br1_3,br2_3,operator1,operator2,vreme,cena;
+    cin>>br1>>br2>>vreme;
+    if(vreme<=30)
+        cena=vreme*3;
+    else if(vreme>30)
+        cena=vreme*3-(vreme-30)*1;
+    br1_3=br1/1000000;
+    br2_3=br2/1000000;
+    operator1=071;
+    operator1=072;
+    operator1=073;
+
+    if(br1_3== && br2_3 == )
+    cout<<cena;
+    return 0;
+}
